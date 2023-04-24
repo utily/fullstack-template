b4ac0650aee19484eca8359d54613ba7f93a8ae4
Elias Eriksson <eliaseriksson95@gmail.com>
2023-04-24 13:54:27 +0200

    proxy  / ASSETS

--- api/Context/index.ts
+++ api/Context/index.ts
@@ -5,21 +5,34 @@ import { router } from "../router"
 import { Environment } from "./Environment"
 
 export class Context {
-	constructor(public readonly environment: Environment) {}
+	constructor(public readonly environment: Environment, private readonly request: http.Request) {}
 	async authenticate(request: http.Request): Promise<"admin" | undefined> {
 		return this.environment.adminSecret && request.header.authorization == `Basic ${this.environment.adminSecret}`
 			? "admin"
 			: undefined
 	}
 	static async handle(request: Request, environment: Environment): Promise<Response> {
-		let result: http.Response
+		let result: Response
 		try {
-			result = await router.handle(http.Request.from(request), new Context(environment))
+			const context = new Context(environment, http.Request.from(request))
+			if (context.request.url.pathname.startsWith("/api"))
+				result = await http.Response.to(await router.handle(context.request, context))
+			else if (environment.ASSETS)
+				result = await environment.ASSETS.fetch(request)
+			else if (environment.ui) {
+				context.request.url.host = environment.ui
+				result = await http.Response.to(await http.fetch(context.request))
+			} else
+				result = await http.Response.to(
+					http.Response.create(
+						gracely.server.misconfigured("ASSETS | ui", "Deploy properly with pages or supply ui url.")
+					)
+				)
 		} catch (e) {
 			const details = (typeof e == "object" && e && e.toString()) || undefined
-			result = http.Response.create(gracely.server.unknown(details, "exception"))
+			result = await http.Response.to(http.Response.create(gracely.server.unknown(details, "exception")))
 		}
-		return http.Response.to(result)
+		return result
 	}
 }
 
