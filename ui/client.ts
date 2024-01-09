efcfdb9e728a0a9454b5962e1375207711e09230
Anton Kroner <anton.kroner@pax2pay.com>
2024-01-09 11:45:58 +0100

    Fixed client url.

--- ui/client.ts
+++ ui/client.ts
@@ -1,3 +1,6 @@
 import { model } from "../model"
 
-export const client = model.Client.create("http://localhost:8787", "")
+export const client = model.Client.create(
+	window.location.origin.includes("localhost") ? "http://localhost:8787" : window.location.origin,
+	""
+)
